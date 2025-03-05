8316fb593802e022da60ac4f8937facba6acc180
Pinto Manuel <pintoydc@gmail.com>
2025-03-05 06:46:12 +0200

    Fixed authentication bug

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import type { NextConfig } from 'next';
-import type { Configuration } from 'webpack';
 
 const nextConfig: NextConfig = {
-  swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -19,25 +17,6 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_KEYCLOAK_URL: process.env.NEXT_PUBLIC_KEYCLOAK_URL,
     NEXT_PUBLIC_KEYCLOAK_REALM: process.env.NEXT_PUBLIC_KEYCLOAK_REALM,
     NEXT_PUBLIC_KEYCLOAK_CLIENT_ID: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID,
-  },
-  webpack: (
-    config: Configuration,
-    { isServer }: { isServer: boolean }
-  ) => {
-    config.cache = {
-      type: 'filesystem',
-      buildDependencies: {
-        config: [__filename],
-      },
-    };
-
-    if (!isServer) {
-      config.resolve = {
-        ...config.resolve,
-        fallback: { fs: false },
-      };
-    }
-    return config;
   },
 };
 
