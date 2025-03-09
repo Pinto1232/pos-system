5763ab8a1157bc02a446b6047623d58469e05cc8
Pinto Manuel <pintoydc@gmail.com>
2025-03-09 05:53:03 +0200

    Commit code

--- src/components/pricing-packages/types.ts
+++ src/components/pricing-packages/types.ts
@@ -1,12 +1,12 @@
 export type PricePackages = {
   data: Array<{
     id: number;
-    title: string;
-    description: string;
-    icon: string;
-    extraDescription: string;
-    price: number;
-    testPeriodDays: number;
+    title?: string;
+    description?: string;
+    icon?: string;
+    extraDescription?: string;
+    price?: number;
+    testPeriodDays?: number;
     type?: string;
     packageType?: string;
   }>;
