719b12db8af8c433ef294135da91ee8e6172d300
Pinto Manuel <pintoydc@gmail.com>
2025-03-18 10:47:33 +0200

    commit changes

--- src/components/dashboard-layout/DashboardLayout.tsx
+++ src/components/dashboard-layout/DashboardLayout.tsx
@@ -3,46 +3,58 @@ import { Box } from "@mui/material";
 import Sidebar from "@/components/sidebar/Sidebar";
 import Navbar from "@/components/sidebar/Navbar";
 
-const drawerWidth = 300; 
+const drawerWidth = 300;
 
 interface DashboardLayoutProps {
   isDrawerOpen: boolean;
   onDrawerToggle: () => void;
-  backgroundColor?: string; 
-  textColor?: string; 
-  iconColor?: string; 
-  navbarBgColor?: string; 
+  backgroundColor?: string;
+  textColor?: string;
+  iconColor?: string;
+  navbarBgColor?: string;
 }
 
-const DashboardLayout: React.FC<DashboardLayoutProps> = ({ isDrawerOpen, onDrawerToggle, backgroundColor, textColor, iconColor, navbarBgColor }) => {
-  return (
-    <Box sx={{ display: "flex", minHeight: "100vh" }}>
-      {/* Sidebar (MUI Drawer) */}
-      <Sidebar
-        drawerWidth={drawerWidth}
-        isDrawerOpen={isDrawerOpen}
-        onDrawerToggle={onDrawerToggle}
-        backgroundColor={backgroundColor}
-        textColor={textColor}
-        iconColor={iconColor} 
-      />
-      {/* Main Section */}
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          display: "flex",
-          flexDirection: "column",
-          mt: 8, 
-          ml: { sm: isDrawerOpen ? `${drawerWidth}px` : '60px' }, 
-        }}
-      >
-        {/* Top Navbar */}
-        <Navbar drawerWidth={isDrawerOpen ? drawerWidth : 60} onDrawerToggle={onDrawerToggle} backgroundColor={navbarBgColor} /> {/* Pass navbarBgColor prop */}
-  
+const DashboardLayout:
+  React.FC<DashboardLayoutProps> = ({
+    isDrawerOpen,
+    onDrawerToggle,
+    backgroundColor,
+    textColor,
+    iconColor,
+    navbarBgColor
+  }) => {
+
+    return (
+      <Box sx={{ display: "flex", minHeight: "100vh" }}>
+        {/* Sidebar (MUI Drawer) */}
+        <Sidebar
+          drawerWidth={drawerWidth}
+          isDrawerOpen={isDrawerOpen}
+          onDrawerToggle={onDrawerToggle}
+          backgroundColor={backgroundColor}
+          textColor={textColor}
+          iconColor={iconColor}
+        />
+        {/* Main Section */}
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+            flexDirection: "column",
+            mt: 8,
+            ml: { sm: isDrawerOpen ? `${drawerWidth}px` : '60px' },
+          }}
+        >
+          {/* Top Navbar */}
+          <Navbar
+            drawerWidth={isDrawerOpen ? drawerWidth : 60}
+            onDrawerToggle={onDrawerToggle}
+            backgroundColor={navbarBgColor}
+          />
+        </Box>
       </Box>
-    </Box>
-  );
-};
+    );
+  };
 
 export default DashboardLayout;
