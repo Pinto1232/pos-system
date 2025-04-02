e41c2d5183d88f90765c4361cf1e8d74ffbc1c69
Pinto Manuel <pintoydc@gmail.com>
2025-04-02 22:35:54 +0200

    commit change on the navbar

--- src/components/sidebar/Navbar.tsx
+++ src/components/sidebar/Navbar.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { AppBar, Box, Toolbar, IconButton, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  Divider
+} from "@mui/material";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import { navbarLinks } from "../../settings";
 import { FaRegBell } from "react-icons/fa";
-import { FiUser } from "react-icons/fi";
+import { FiUser, FiSettings, FiLogOut } from "react-icons/fi";
 
 interface NavbarProps {
   drawerWidth: number;
@@ -13,6 +23,22 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ drawerWidth, onDrawerToggle, backgroundColor }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    // Add your logout logic here
+    handleClose();
+  };
+
   return (
     <AppBar
       position="fixed"
@@ -68,9 +94,61 @@ const Navbar: React.FC<NavbarProps> = ({ drawerWidth, onDrawerToggle, background
               fontSize: "1.9rem",
             }} />
           </Typography>
-          <Typography variant="body1" sx={{ cursor: "pointer" }}>
+          <Typography
+            variant="body1"
+            sx={{ cursor: "pointer" }}
+            onClick={handleClick}
+          >
             Profile
           </Typography>
+          <Menu
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            sx={{
+              marginTop: '25px',
+              '& .MuiPaper-root': {
+                minWidth: '200px'
+              },
+              '& .MuiList-root': {
+                padding: 0
+              }
+            }}
+          >
+            <MenuItem onClick={handleClose}>
+              <ListItemIcon>
+                <FiUser fontSize="small" />
+              </ListItemIcon>
+              My Profile
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <ListItemIcon>
+                <FiSettings fontSize="small" />
+              </ListItemIcon>
+              Settings
+            </MenuItem>
+            <MenuItem
+              onClick={handleLogout}
+              sx={{
+                backgroundColor: '#173a79',
+                color: 'white',
+                padding: '8px 16px',
+                margin: 0,
+                borderRadius: 0,
+                '&:hover': {
+                  backgroundColor: '#1a4589'
+                }
+              }}
+            >
+              <ListItemIcon>
+                <FiLogOut fontSize="small" style={{ color: 'white' }} />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
         </Box>
       </Toolbar>
     </AppBar>
